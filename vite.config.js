ff93b01eed62f3ee7f4f0e188a1218105973590e
hashem <hashemalsaie0457@gmail.com>
2026-05-16 14:31:18 +0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from "@vitejs/plugin-react";
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: command === "build" ? "/ILM-StartUp/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        index: new URL("./index.html", import.meta.url).pathname,
+        ilm: new URL("./ilm.html", import.meta.url).pathname,
+      },
+    },
+  },
 }));
